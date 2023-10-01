323e3a046cd72a164ce82dfb662af5715ada6e28
KalitaAS <146657940+KalitaAS@users.noreply.github.com>
2023-10-01 21:54:08 +0500

    Update calcu.py

--- calc2/venv/calcu.py
+++ calc2/venv/calcu.py
@@ -1,5 +1,3 @@
-print("Данный калькулятор умеет складывать, вычитать, умножать и делить два числа.")
-
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -18,6 +16,22 @@ def subtract():
     num1 = data['num1']
     num2 = data['num2']
     result = num1 - num2
+    return jsonify({'result': result})
+
+@app.route('/multiply', methods=['POST'])
+def multiply():
+    data = request.get_json()
+    num1 = data['num1']
+    num2 = data['num2']
+    result = num1 * num2
+    return jsonify({'result': result})
+
+@app.route('/divide', methods=['POST'])
+def divide():
+    data = request.get_json()
+    num1 = data['num1']
+    num2 = data['num2']
+    result = num1 / num2
     return jsonify({'result': result})
 
 if __name__ == '__main__':
